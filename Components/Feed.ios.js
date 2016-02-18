f6d99dc9b6b5071d662781563b78c94da88d2e3c
Joel Trew <joel@3sidedcube.com>
2016-02-18 18:59:04 +0000

    Changes Navigation to NavigationIOS, and cleans up code

--- Components/Feed.ios.js
+++ Components/Feed.ios.js
@@ -44,26 +44,22 @@ var Feed = React.createClass({
       .then((response) => response.json())
       .then((responseData) => this.filterNews(responseData.relations))
       .then((newsItems) => 
-      {
-        console.log(newsItems);
-        this.setState({isAnimating: false})
-        
-        setTimeout(() => {
+      { 
           this.setState({
             dataSource: this.state.dataSource.cloneWithRows(newsItems),
             loaded: true,
-            isRefreshing: false
+            isRefreshing: false,
+            isAnimating: false
+          })
 
-          });
-        },200)
-      })
-      .done();
-    },
+      
+    }).done();
+  },
 
     renderLoading() {
       return (
         <View style={{flexDirection: 'row', justifyContent: 'center', flex: 1}}>
-        <Loader isAnimating={this.state.isAnimating} lineWidth={10} color={'#ff00ff'} style={styles.loadingView}/>
+          <Loader isAnimating={this.state.isAnimating} lineWidth={10} color={'#ff00ff'} style={styles.loadingView}/>
         </View>
         );
     },
@@ -72,14 +68,6 @@ var Feed = React.createClass({
       return (
         <Story story={story} navigator={this.props.navigator}></Story>
         );
-    },
-
-    handleScroll(event) {
-      if(event.nativeEvent.contentOffset.y > 40) {
-        this.props.changeNavBarHeight(true);
-      } else {
-        this.props.changeNavBarHeight(false);
-      }
     },
 
     render: function() {
@@ -93,8 +81,7 @@ var Feed = React.createClass({
         dataSource={this.state.dataSource}
         renderRow={this.renderStories}
         style={styles.listView}
-        contentInset={{top:60,left:0,right:0,bottom:0}}
-        contentOffset={{x:0, y:-60}}
+        contentInset={{top: 0, left: 0, bottom: 64, right: 0} }
  
         scrollEventThrottle={200}
         {...this.props}
@@ -102,9 +89,8 @@ var Feed = React.createClass({
           <RefreshControl
           refreshing={this.state.isRefreshing}
           onRefresh={this.fetchData}
-          tintColor="#ff0000"
+          tintColor="#bb1919"
           title="Loading..."
-          colors={['#ff0000', '#00ff00', '#0000ff']}
           progressBackgroundColor="#ffff00"
 
           />
